5998d4ff4febf9070e17b2959cad9c9fb3ab2db2
M SANJEEV <98035905+sanjureddysj@users.noreply.github.com>
2024-08-27 16:15:14 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./../public/styles.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//CHALLENGE: Make the code in App.jsx work.
+//The final app should have a single contact
+//with fName, lName and email.
